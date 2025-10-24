a6331a9bae52a84f8319ddc35e6744dfe5b2a78f
José Domingo Muñoz <josedom24@gmail.com>
2025-10-24 21:00:46 +0200

    guestbook

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// Archivo: config.php
+// Configuración de conexión a MariaDB/MySQL
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'guestbook',
+        'user' => 'tu_usuario',
+        'pass' => 'tu_contraseña',
+        'charset' => 'utf8mb4',
+    ],
+];
